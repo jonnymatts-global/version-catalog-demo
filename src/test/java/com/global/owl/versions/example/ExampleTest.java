e2f9cd12e38af1a80b5334c1545dcbe30f52caf1
jonnymatts-global <jonathan.matts@global.com>
2024-10-07 11:32:13 +0100

    example: libs.versions.toml

---
+++ src/test/java/com/global/owl/versions/example/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.global.owl.versions.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void ifThisFailsWeReallyNeedToWorry() {
+
+        assertThat(true).isTrue();
+    }
+}
